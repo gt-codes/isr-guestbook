bbd09092a9ea60af353de2d9a195ea4cb5213d84
Garrett Tolbert <garrett1.tolbert@gmail.com>
2022-01-16 19:50:30 -0600

    feat: created auth provider for app

---
+++ hooks/useAuth.tsx
@@ -0,0 +1,63 @@
+import { Session } from '@supabase/supabase-js';
+import {
+	createContext,
+	Dispatch,
+	ReactNode,
+	SetStateAction,
+	useContext,
+	useEffect,
+	useState,
+} from 'react';
+import { supabase } from '@/utils/supabase';
+
+interface initialProps {
+	session: Session | null;
+	signInWithTwitter: () => Promise<any>;
+	setSession: Dispatch<SetStateAction<Session | null>>;
+}
+
+const initialState: initialProps = {
+	session: null,
+	setSession: () => null,
+	signInWithTwitter: async () => {},
+};
+
+const AuthContext = createContext(initialState);
+
+interface LayoutProps {
+	children: ReactNode;
+}
+export function AuthProvider({ children }: LayoutProps): JSX.Element {
+	const data = useAuthLayer();
+	return <AuthContext.Provider value={data}>{children}</AuthContext.Provider>;
+}
+
+export const useAuth = (): initialProps => {
+	return useContext(AuthContext);
+};
+
+const useAuthLayer = () => {
+	const [session, setSession] = useState<Session | null>(
+		supabase.auth.session()
+	);
+	console.log({ session });
+
+	const signInWithTwitter = async () => {
+		const { session } = await supabase.auth.signIn({
+			provider: 'twitter',
+		});
+		setSession(session);
+	};
+
+	useEffect(() => {
+		supabase.auth.onAuthStateChange((event, session) => {
+			console.log({ event, session });
+		});
+	});
+
+	return {
+		session,
+		setSession,
+		signInWithTwitter,
+	};
+};
