947f3401ad9a418a54f7609db7cf713a7d108e05
Garrett Tolbert <garrett1.tolbert@gmail.com>
2022-01-17 15:02:09 -0600

    refactor: extracted message component

---
+++ components/Message.tsx
@@ -0,0 +1,67 @@
+import { Stack, Image, Text, HStack } from '@chakra-ui/react';
+import { format } from 'date-fns';
+import Link from 'next/link';
+import { Message as MessageType } from '@/types/index';
+import { useRouter } from 'next/router';
+
+interface Props {
+	message: MessageType;
+}
+
+export default function Message({ message }: Props) {
+	const router = useRouter();
+	const isHome = router.pathname === '/';
+
+	const handleClick = () => {
+		if (!isHome) return;
+		router.push(`/guest/${message.ownerId}`);
+	};
+
+	return (
+		<Stack
+			as="a"
+			p={!isHome ? 0 : 2}
+			spacing={1}
+			cursor={!isHome ? 'default' : 'pointer'}
+			rounded="md"
+			onClick={handleClick}
+			transition="all 0.2s"
+			_hover={!isHome ? {} : { bg: '#f5f5f5' }}
+		>
+			<Text fontSize="xl" color="gray.600" fontWeight="medium">
+				{message.message}
+			</Text>
+			<HStack alignItems="center" spacing={1}>
+				<Link passHref href={`https://twitter.com/${message.username}`}>
+					<HStack
+						as="a"
+						target="_blank"
+						alignItems="center"
+						rounded="full"
+						pl="1px"
+						pr={1}
+						spacing={1}
+						w="fit-content"
+						transition="all 0.2s"
+						cursor="pointer"
+						_hover={{ bg: 'gray.100' }}
+					>
+						<Image src={message.avi} alt="avatar" rounded="full" h={4} w={4} />
+						<Text fontSize="xs" color="gray.600">
+							{message.username}
+						</Text>
+					</HStack>
+				</Link>
+				<Text fontSize="xs" color="gray.600">
+					•
+				</Text>
+				<HStack color="gray.500" spacing={1}>
+					<Text fontSize="xs" color="gray.600">
+						{format(new Date(message.created_at), 'MMM d, yyyy h:mm a')}
+						{/* {formatDistanceToNow(new Date(message.created_at))} */}
+					</Text>
+				</HStack>
+			</HStack>
+		</Stack>
+	);
+}
--- components/MessageList.tsx
+++ components/MessageList.tsx
@@ -1,10 +1,11 @@
-import { Image, Text, HStack, Stack } from '@chakra-ui/react';
-import { format } from 'date-fns';
+import { Stack } from '@chakra-ui/react';
 import Link from 'next/link';
 import { useState, useEffect } from 'react';
+import { Message as MessageType } from '../types';
+import Message from './Message';
 
 export default function MessageList() {
-	const [messages, setMessages] = useState<any[]>([]);
+	const [messages, setMessages] = useState<MessageType[]>([]);
 
 	useEffect(() => {
 		const getMessages = async () => {
@@ -26,48 +27,7 @@ export default function MessageList() {
 			overflowY="scroll"
 		>
 			{messages.map((message) => (
-				<Stack key={message.id} spacing={1}>
-					<Text fontSize="xl" color="gray.600" fontWeight="medium">
-						{message.message}
-					</Text>
-					<HStack alignItems="center" spacing={1}>
-						<Link passHref href={`https://twitter.com/${message.username}`}>
-							<HStack
-								as="a"
-								target="_blank"
-								alignItems="center"
-								rounded="full"
-								pl="1px"
-								pr={1}
-								spacing={1}
-								w="fit-content"
-								transition="all 0.2s"
-								cursor="pointer"
-								_hover={{ bg: 'gray.100' }}
-							>
-								<Image
-									src={message.avi}
-									alt="avatar"
-									rounded="full"
-									h={4}
-									w={4}
-								/>
-								<Text fontSize="xs" color="gray.600">
-									{message.username}
-								</Text>
-							</HStack>
-						</Link>
-						<Text fontSize="xs" color="gray.600">
-							•
-						</Text>
-						<HStack color="gray.500" spacing={1}>
-							<Text fontSize="xs" color="gray.600">
-								{format(new Date(message.created_at), 'MMM d, yyyy h:mm a')}
-								{/* {formatDistanceToNow(new Date(message.created_at))} */}
-							</Text>
-						</HStack>
-					</HStack>
-				</Stack>
+				<Message message={message} key={message.id} />
 			))}
 		</Stack>
 	);
