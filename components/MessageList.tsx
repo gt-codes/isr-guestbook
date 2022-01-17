a5aef1858a9b74f487d775fa791af868d889e34d
Garrett Tolbert <garrett1.tolbert@gmail.com>
2022-01-17 01:39:07 -0600

    feat: added messages to home page

---
+++ components/MessageList.tsx
@@ -0,0 +1,79 @@
+import { useAuth } from '@/hooks/useAuth';
+import { Image, Text, HStack, Stack } from '@chakra-ui/react';
+import { format, formatDistanceToNow } from 'date-fns';
+import Link from 'next/link';
+import { useState, useEffect } from 'react';
+import { HiOutlineClock } from 'react-icons/hi';
+
+export default function MessageList() {
+	const [messages, setMessages] = useState<any[]>([]);
+	const { session, signInWithTwitter } = useAuth();
+
+	console.log({ messages });
+
+	useEffect(() => {
+		const getMessages = async () => {
+			const res = await fetch('/api/getMessages');
+			const data = await res.json();
+			setMessages(data);
+		};
+
+		session && getMessages();
+	}, [session]);
+
+	return (
+		<Stack
+			mt={12}
+			flexGrow={1}
+			px={{ base: 4, md: 24 }}
+			pb={12}
+			spacing={6}
+			overflowY="scroll"
+		>
+			{messages.map((message) => (
+				<Stack key={message.id} spacing={1}>
+					<Text fontSize="xl" color="gray.600" fontWeight="medium">
+						{message.message}
+					</Text>
+					<HStack alignItems="center" spacing={1}>
+						<Link passHref href={`https://twitter.com/${message.username}`}>
+							<HStack
+								as="a"
+								target="_blank"
+								alignItems="center"
+								rounded="full"
+								pl="1px"
+								pr={1}
+								spacing={1}
+								w="fit-content"
+								transition="all 0.2s"
+								cursor="pointer"
+								_hover={{ bg: 'gray.100' }}
+							>
+								<Image
+									src={message.avi}
+									alt="avatar"
+									rounded="full"
+									h={4}
+									w={4}
+								/>
+								<Text fontSize="xs" color="gray.600">
+									{message.username}
+								</Text>
+							</HStack>
+						</Link>
+						<Text fontSize="xs" color="gray.600">
+							•
+						</Text>
+						<HStack color="gray.500" spacing={1}>
+							<Text fontSize="xs" color="gray.600">
+								{format(new Date(message.created_at), 'MMM d, yyyy h:mm a')}
+								{/* {formatDistanceToNow(new Date(message.created_at))} */}
+							</Text>
+						</HStack>
+					</HStack>
+				</Stack>
+			))}
+		</Stack>
+	);
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
-import { Button, Text, Flex, Heading, Stack, Center } from '@chakra-ui/react';
+import { Button, Flex, Heading, Center } from '@chakra-ui/react';
 import Router from 'next/router';
 import MessageInput from '@/components/MessageInput';
 import { useAuth } from '@/hooks/useAuth';
 import { supabase } from '@/utils/supabase';
+import { BsTwitter } from 'react-icons/bs';
+import MessageList from '@/components/MessageList';
+import { FormEvent } from 'react';
 
 export default function Home() {
 	const { session, signInWithTwitter } = useAuth();
@@ -12,21 +15,59 @@ export default function Home() {
 		Router.reload();
 	};
 
+	const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		const { message } = e.target as any;
+		if (!message.value) return;
+
+		const res = await fetch('/api/addMessage', {
+			method: 'POST',
+			body: JSON.stringify({
+				message: message.value,
+				ownerId: session?.user?.id,
+				avi: session?.user?.user_metadata.picture,
+				username: session?.user?.user_metadata.user_name,
+			}),
+		});
+		await res.json();
+		message.value = '';
+	};
+
 	return (
 		<Center w="100vw" h="100vh" alignItems="start">
-			<Flex flexDir="column" w="full" maxW="3xl" p={{ base: 6, md: 24 }}>
-				<Heading textAlign="center">ISR Guestbook</Heading>
+			<Flex
+				flexDir="column"
+				w="full"
+				h="full"
+				maxW="3xl"
+				pt={{ base: 6, md: 24 }}
+			>
+				<Heading
+					textAlign="center"
+					bgGradient="linear(to-r, #42A5F5, #0D47A1)"
+					bgClip="text"
+					fontSize="5xl"
+					fontWeight="black"
+					mb={12}
+				>
+					ISR Guestbook
+				</Heading>
 				{!session ? (
-					<Button onClick={signInWithTwitter} w="fit-content" mt={12}>
-						Sign in
+					<Button
+						onClick={signInWithTwitter}
+						w="fit-content"
+						margin="auto"
+						colorScheme="twitter"
+						leftIcon={<BsTwitter />}
+					>
+						Sign in with Twitter
 					</Button>
 				) : (
 					<>
-						<MessageInput />
-
-						<Button onClick={logout} w="fit-content" mt={12}>
-							Logout
-						</Button>
+						<form onSubmit={handleSubmit}>
+							<MessageInput />
+						</form>
+						<MessageList />
 					</>
 				)}
 			</Flex>
