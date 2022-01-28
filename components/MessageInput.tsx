00b01ff92edee6a7da0e8cea182f528cab5a0d86
Garrett Tolbert <garrett1.tolbert@gmail.com>
2022-01-27 18:42:43 -0600

    refactor: removed image from message input

--- components/MessageInput.tsx
+++ components/MessageInput.tsx
@@ -1,42 +1,26 @@
 import {
 	FormControl,
 	Text,
-	Image,
 	FormLabel,
 	InputGroup,
-	InputLeftElement,
 	Input,
 	FormHelperText,
 	InputRightElement,
 	Button,
 } from '@chakra-ui/react';
 import { HiOutlineShieldCheck } from 'react-icons/hi';
-import { useAuth } from '@/hooks/useAuth';
 
 export default function MessageInput() {
-	const { session } = useAuth();
-
 	return (
 		<FormControl mt={4} px={{ base: 4, md: 24 }}>
 			<FormLabel htmlFor="message" color="gray.600" fontWeight="medium">
 				Share a message for others to see
 			</FormLabel>
 			<InputGroup>
-				<InputLeftElement>
-					<Image
-						h={8}
-						w={8}
-						objectFit="cover"
-						rounded="full"
-						alt="twitter avatar"
-						src={session?.user?.user_metadata.picture}
-					/>
-				</InputLeftElement>
 				<Input
 					rounded="full"
 					id="message"
 					type="text"
-					pl={12}
 					pr={20}
 					placeholder="Your message"
 				/>
